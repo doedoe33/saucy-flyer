00e55b9a1c7baeba45ff1ae7055a7249d4b7c441
rmar72 <rsolm72@gmail.com>
2019-06-04 18:38:28 -0600

    new gallery design implemented (NOT responsive)

--- js/news.js
+++ js/news.js
@@ -1,10 +1,10 @@
-$("#slideshow > div:gt(0)").hide();
+$(".slideshow > div:gt(0)").hide();
 
 setInterval(function() { 
-  $('#slideshow > div:first')
+  $('.slideshow > div:first')
     .fadeOut(1000)
     .next()
     .fadeIn(1000)
     .end()
-    .appendTo('#slideshow');
+    .appendTo('.slideshow');
 },  3000);
