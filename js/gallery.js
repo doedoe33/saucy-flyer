b8014eca3a2590dd5b60dac385f6b2156b26e0a6
rmar72 <rsolm72@gmail.com>
2019-06-12 15:27:24 -0600

    desktop gallery photo slider

---
+++ js/gallery.js
@@ -0,0 +1,41 @@
+$(window).on('load', function(){
+
+  // --- carousel slider
+  var owl = $('.owl-carousel');
+  
+  owl.owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: [
+      "<i class='fas fa-chevron-left fa-3x'></i>",
+      "<i class='fas fa-chevron-right fa-3x'></i>"
+    ],
+    autoplay: false,
+    autoplayHoverPause: true,
+    dots: true,
+    dotsData: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  });
+
+  // --- mousewheel
+  // owl.on('mousewheel', '.owl-stage', function (e) {
+  //     if (e.originalEvent.deltaY > 0) {
+  //         owl.trigger('next.owl');
+  //     } else {
+  //         owl.trigger('prev.owl');
+  //     }
+  //     e.preventDefault();
+  // });
+
+});
