d0c686c39e27a541210db4855e68bd557a681ebd
rmar72 <rsolm72@gmail.com>
2020-01-08 00:03:10 -0700

    better naming convention and fixed view bugs for desktop

--- js/story.js
+++ js/story.js
@@ -1,11 +1,11 @@
 $(window).on('load', function(){
-    const whiteScroller = document.querySelector(".scrollable-paragraph");  
+    const paraScroller = document.querySelector(".scrollable-paragraph");  
     let executeAutoScroll;
 
 
     function autoScrollDown(){
-        whiteScroller.scrollTop+=1;
-        if(whiteScroller.scrollTop === 560){
+        paraScroller.scrollTop+=1;
+        if(paraScroller.scrollTop === 560){
             stopFunc();
         }
     }
@@ -15,23 +15,28 @@ $(window).on('load', function(){
     }
 
     // created bug on scroller, made it slow, but handled it with beneath code
-    whiteScroller.addEventListener("touchstart", (event) => {
+    paraScroller.addEventListener("touchstart", (event) => {
         stopFunc();
     });
 
 
-    // handles running script only when looking at a media query; well in this case ipad & ipad pro media query
-    // problem was that this story.js script was running the touchstart even on desktop and that became a bug on the scroller
-    function myFunction(x) {
-        if (x.matches) { // If media query matches
+    // this whole story.js script runs only if it matches ipad and ipad pro landscapes
+    // problem was that this story.js script was running the touchstart event even on desktop and that became a bug on the scroller
+    function startScroller(device) {
+        if (device.matches) { // If device media query matches start the scroller
             setTimeout( () => {
                 executeAutoScroll = setInterval(autoScrollDown, 50);
           }, 2000);
         }
     }
 
-    var x = window.matchMedia("only screen and (min-width: 1024px) and (max-height: 1366px)");
-    myFunction(x) // Call listener function at run time
-    x.addListener(myFunction) 
+    var ipad = window.matchMedia("only screen and (min-width: 1023px) and (max-width: 1024px) and (min-height: 767px)and (max-height: 768px) and (orientation : landscape)");
+    var ipadpro = window.matchMedia("only screen and (min-width: 1365px) and (max-width: 1366px) and (min-height: 1023px) and (max-height: 1024px) and (orientation : landscape)");
+    
+    startScroller(ipad) // Call listener function at run time
+    // ipad.addListener(startScroller) // not needed apparently?!
+
+    startScroller(ipadpro) // Call listener function at run time
+    // ipadpro.addListener(startScroller)  // not needed apparently?!
 
 });
